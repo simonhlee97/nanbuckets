c174d78b9a18605a2b3f35c9e52c49444db6c328
simonhlee97 <simonhlee97@hotmail.com>
2017-09-18 10:19:34 -0400

    module 2

--- server/index.js
+++ server/index.js
@@ -1,1 +1,15 @@
-console.log('server side is working');
+var express = require('express');
+var http = require('http');
+var bodyParser = require('body-parser');
+var app = express();
+
+app.use(bodyParser.json({ type: '*/*'}));
+
+var port = process.env.PORT || 3000;
+var server = http.createServer();
+
+var server = http.createServer(app);
+
+server.listen(port);
+console.log('Server is listening on ' + port);
+
