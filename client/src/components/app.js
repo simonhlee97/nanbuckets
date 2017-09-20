929996e9ceb063aa6f13cb08686556ea5ef9d80d
simonhlee97 <simonhlee97@hotmail.com>
2017-09-20 10:09:19 -0400

    mod 11 part 2 done

--- client/src/components/app.js
+++ client/src/components/app.js
@@ -1,9 +1,15 @@
 import React, { Component } from 'react';
+import NavBarHeader from './Nav';
+import Video from './video/video'
 
 export default class App extends Component {
 	render() {
-		return(
-			<div>React Hello World</div>
+		return (
+
+			<div>
+				<NavBarHeader />
+				<Video />
+			</div>
 		);
 	}
 }
---
+++ client/src/components/video/video.js
@@ -0,0 +1,38 @@
+import _ from 'lodash';
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
+import SearchBar from './search-bar';
+import VideoDetail from './video-detail';
+
+const API_KEY = 'AIzaSyDFhU0zo8qxHuqgDKc9YHfIhw6_EaZ5Uxw';
+
+class Video extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			videos: [],
+			selectedVideo: null
+		};
+		this.videoSearch('bucketlist');
+	}
+	videoSearch(term) {
+		YTSearch({key: API_KEY, term: term}, (videos) => {
+			this.setState({
+				videos: videos,
+				selectedVideo: videos[0]
+			});
+		});
+	}
+	render() {
+		const videoSearch = _.debounce((term)=>{ this.videoSearch(term) }, 1800);
+		return (
+			<div>
+				<SearchBar onSearchTermChange={videoSearch} />
+				<VideoDetail video={this.state.selectedVideo}/>
+			</div>
+		);
+	}
+}
+
+export default Video;
