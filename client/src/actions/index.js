481cbee6240743e8d6ebd5e98ca6577b7b5d6158
simonhlee97 <simonhlee97@hotmail.com>
2017-09-23 14:57:13 -0400

    mod 19 done

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -36,3 +36,9 @@ export function AuthError(error) {
 		payload: error
 	};
 }
+
+export function signoutUser(){
+	localStorage.removeItem('token');
+
+	return {type: UNAUTH_USER};
+}
--- client/src/index.js
+++ client/src/index.js
@@ -3,19 +3,28 @@ import ReactDOM from 'react-dom';
 import reduxThunk from 'redux-thunk';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
-import { Router, Route, IndexRoute, browserHistory } from 'react-router';
-
+import { Router, Route, IndexRoute, browserHistory} from 'react-router';
+// components
 import App from './components/app';
+import Signin from './components/auth/signin';
+import Signout from './components/auth/signout';
+import Signup from './components/auth/signup';
+import Feature from './components/feature';
+import RequireAuth from './components/auth/require_auth';
+// reducers
 import reducers from './reducers';
 
 var createStoreWithMiddleware = applyMiddleware(reduxThunk)(createStore);
 
 ReactDOM.render(
-	<Provider store={createStoreWithMiddleware(reducers)}>
+  <Provider store={createStoreWithMiddleware(reducers)}>
 		<Router history={browserHistory}>
-			<Route path="/" component={App}>
-			</Route>
-		</Router>
-	</Provider>
-	, document.querySelector('.container'));
-
+  			<Route path="/" component={App}>
+    			<Route path="signin" component={Signin} />
+   				<Route path="signout" component={Signout} />
+    			<Route path="signup" component={Signup} />
+    			<Route path="feature" component={RequireAuth(Feature)} />
+        </Route>
+  	</Router>
+  </Provider>
+  , document.querySelector('.container'));
