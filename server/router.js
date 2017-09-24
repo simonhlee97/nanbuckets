354a0b68869ebb397a2ac2aa6875f8a3168d1bf1
simonhlee97 <simonhlee97@hotmail.com>
2017-09-24 09:52:22 -0400

    mod 25 done

--- server/router.js
+++ server/router.js
@@ -14,5 +14,6 @@ module.exports = function(app){
 	app.post('/signin', requireSignin, Auth.signin);
 	app.post('/newitem', requireAuth, BucketList.addBucketList);
 	app.get('/items', requireAuth, BucketList.fetchBucketLists);
+	app.delete('/items/:id', requireAuth, BucketList.deleteBucketLists);
 }
 
